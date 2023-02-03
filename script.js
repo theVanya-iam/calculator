b41dca7f452c2f1696c9e80996d523fa02d58410
theVanya-iam <48164993+theVanya-iam@users.noreply.github.com>
2023-02-03 18:20:37 -0500

    rewrite functions and clean code

--- script.js
+++ script.js
@@ -6,16 +6,16 @@ const control_percent = document.querySelector("#control_percent");
 const control_float = document.querySelector("#control_float");
 const control_equal = document.querySelector("#control_equal");
 
-let selected_operator = 0;
-let first_operand = 0;
-let second_operand = 0;
+let selected_operator = null;
+let first_operand = null;
+let second_operand = null;
 let display_value = 0;
-screen.textContent = 0;
+screen.textContent = display_value;
 
 function clearScreen(){
-    selected_operator = 0;
-    first_operand = 0;
-    second_operand = 0;
+    selected_operator = null;
+    first_operand = null;
+    second_operand = null;
     display_value = 0;
     screen.textContent = 0;
 }
@@ -29,7 +29,7 @@ function operate(x, y, operator){
         return x - y;
     } else if (operator == '/'){
         if (y === 0){
-            return 'lol no';
+            return;
         } else {
             return x / y;
         }
@@ -41,30 +41,52 @@ function operate(x, y, operator){
 }
 
 function floatPoint(){
-    if (display_value.includes('.')){
-        return;
-    } else if (display_value == first_operand){
-        first_operand += '.';
+    if (display_value === 0){
         display_value += '.';
-        screen.textContent = display_value;
-    } else if (display_value == second_operand){
-        second_operand += '.';
-        display_value += '.';
-        screen.textContent = display_value;
+        first_operand = display_value;
+    } else if (first_operand != null && selected_operator === null){
+        if (first_operand.includes('.')){
+            return;
+        } else {
+            first_operand += '.';
+            display_value = first_operand;
+        }
+    } else if (selected_operator != null && second_operand === null){
+        second_operand = '0.';
+        display_value += second_operand;
+
+    } else if (second_operand != null){
+        if (second_operand.includes('.')){
+            return;
+        } else {
+            second_operand += '.';
+            display_value += '.';
+        }
     }
+    updateScreen();
 }
 
 function roundMe(num, places){
     return parseFloat(Math.round(num + 'e' + places) + 'e-' + places);
 }
 
+function changeSign(num){
+    display_value = (num * -1).toString();
+    updateScreen();
+}
+
+function addPercent(num){
+    display_value = (num * -1).toString();
+    updateScreen();
+}
+
 control_clear.addEventListener('click', () => clearScreen());
-control_opposite.addEventListener('click', () => screen.textContent = -(screen.textContent));
-control_percent.addEventListener('click', () => screen.textContent = parseFloat(screen.textContent) * 0.01);
+control_opposite.addEventListener('click', () => changeSign());
+control_percent.addEventListener('click', () => addPercent());
 control_float.addEventListener('click', () => floatPoint());
 
 function operatorSelect(i){
-    if (first_operand){
+    if (first_operand && second_operand === null){
         if (buttons[i].textContent == '/'){
             selected_operator = '/';
         } else if (buttons[i].textContent == '*'){
@@ -74,42 +96,45 @@ function operatorSelect(i){
         } else if (buttons[i].textContent == '-'){
             selected_operator = '-';
         }
+        updateScreen();
     }
+    else { 
+        return;
+    }   
 }
 
-function numOperations(i){
-    if (selected_operator == '0'){
-        if (first_operand == '0'){
-            first_operand = buttons[i].textContent;
-        } else {
-            first_operand += buttons[i].textContent;
-        }        
-    } else if (second_operand == '0'){
+function selectOperand(i){
+    if (first_operand === null){
+        first_operand = buttons[i].textContent;
+    } else if (selected_operator === null && second_operand === null) {
+        first_operand += buttons[i].textContent;
+    } else if (selected_operator != null && second_operand === null){
         second_operand = buttons[i].textContent;
-    } else{
+    } else {
         second_operand += buttons[i].textContent;
     }
 }
 
 function updateScreen(){
-    if (display_value == 0 && first_operand){
+    if (display_value === 0){
         display_value = first_operand;
-    } else if (selected_operator == '0'){
+    } else if (selected_operator === null){
         display_value = first_operand;
-    } else if (selected_operator && second_operand == 0){
+    } else if (selected_operator && second_operand === null){
         if (!display_value.endsWith(selected_operator)){
-        display_value += selected_operator;
+            display_value = first_operand + selected_operator
         }
     } else if (second_operand){
         display_value = first_operand + selected_operator + second_operand;
     }
     screen.textContent = display_value;
+    console.log({display_value});
 }
 
 for (let i=0; i < buttons.length; i++){
     if (buttons[i].classList.contains('number', 'zero')){
         buttons[i].addEventListener('click', () => {
-            numOperations(i);
+            selectOperand(i);
             updateScreen();
         });
     } else if (buttons[i].classList.contains('operator')){
@@ -121,13 +146,13 @@ for (let i=0; i < buttons.length; i++){
 }
 
 control_equal.addEventListener('click', () => {
-    console.log({selected_operator});
-    if (first_operand && second_operand && selected_operator){
+    if (second_operand == 0){
+        screen.textContent = 'Oh. Not again.'
+    } else if (first_operand && second_operand && selected_operator){
         screen.textContent = roundMe(operate(first_operand, second_operand, selected_operator), 2);
         first_operand = screen.textContent;
-        selected_operator = 0;
-        second_operand = 0;
-        updateScreen();
+        selected_operator = null;
+        second_operand = null;
     }
 });
 
